ceb475f32740ca793bc6933085b0e462398b5ad8
OcampoEmiliano <ocampoemilianopa@gmail.com>
2024-05-06 11:09:54 -0300

    trabajo practico finalizado

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,27 @@
+let dia = parseInt(prompt("ingrese un número"));
+switch (dia) {
+    case 1:
+        console.log ("lunes")
+        break;
+    case 2 :
+        console.log ("martes")
+        break;
+    case 3 :
+        console.log ("miercoles")
+        break;
+    case 4 :
+        console.log ("jueves")
+        break;
+    case 5 :
+        console.log ("viernes")
+        break;
+    case 6 :
+        console.log ("sabado")
+        break;
+    case 7 :
+        console.log ("domingo")
+        break;
+    default:
+        console.log ("el número ingresado no es valido")
+        break;
+}
