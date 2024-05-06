c26418579303920e4ee812d14380621be75a2929
OcampoEmiliano <ocampoemilianopa@gmail.com>
2024-05-06 09:32:22 -0300

    parOIpar.js terminado

---
+++ assets/js/ParOImpar.js
@@ -0,0 +1,6 @@
+let edad = parseInt(prompt("ingrese el número"));
+if (edad % 2 == 0) {
+    console.log ("el número ingresado es par")
+} else {
+    console.log ("el número ingresado es impar")
+}
